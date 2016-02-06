3201fec9ded29b5c92695448baab29039ea038fb
zakotm <Zakotm@Zakarias-MacBook-Pro-1207.local>
2016-02-06 16:38:04 -0600

    add test case

---
+++ trial.asm
@@ -0,0 +1,27 @@
+.ORIG x3000
+    ADD R1, R1, #3
+    ADD R1, R1,   #-2
+    ADD R0, R1, #6
+    AND R1, R1, R0
+    AND R0, R0, x2
+    NOT R1, R0
+    AND R0, R0, #0
+    AND R1, R1, #0
+    ADD R1, R1, #4
+    ADD R0, R0, #8
+    LSHF R5, R1, #5
+    RSHFL R6, R0, #1
+    RSHFA R7, R0, #3
+    XOR R7, R1, R0
+    XOR R7, R1, x2
+    LEA R3, A
+    LEA R4, Y
+    STB R3, R4, #0
+    STW R3, R4, #1
+    LDB R0, R4, #0
+    LDW R1, R4, #1
+    HALT
+Y	.FILL x2000
+A	.FILL x1234
+	.END
+
